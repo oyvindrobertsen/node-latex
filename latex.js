dab144d54ac5f0d9fb686d00929d19db1678f826
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-05-11 23:21:58 +0200

    add before and after to test

--- latex.js
+++ latex.js
@@ -17,4 +17,4 @@ exports.compile = function(path, callback) {
             }
         });
     });
-}
+};
--- test/test.js
+++ test/test.js
@@ -1,10 +1,30 @@
-var lt = require('../latex'),
-    assert = require('assert');
+var assert = require('assert'),
+    lt = require('../latex'),
+    fs = require('fs');
 
-var path = __dirname + '/testfiles/test.tex';
+
+var path = __dirname + '/test.tex';
+
+var removeTexCompiledFiles = function() {
+    fs.exists(__dirname + '/test.pdf', function(exists) {
+        if (!exists) {
+            return;
+        }
+        var unlinkCallback = function(err) {
+            if (err) {
+                throw err;
+            }
+        };
+        fs.unlink(__dirname + '/test.pdf', unlinkCallback);
+        fs.unlink(__dirname + '/test.aux', unlinkCallback);
+        fs.unlink(__dirname + '/test.log', unlinkCallback);
+    });
+};
 
 describe('#compile', function() {
+    before(removeTexCompiledFiles);
     it('should compile our latex file', function(done) {
         lt.compile(path, done);
-    })
+    });
+    after(removeTexCompiledFiles);
 });
